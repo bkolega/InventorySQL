37e370dba4515e0b228a7cd80ef305715642f922
mdesry <mdesry@gmail.com>
2015-12-10 09:39:41 -0600

    inventory updates

--- php/sale.php
+++ php/sale.php
@@ -33,6 +33,7 @@ $sold = $_POST["sold"];
 function executeSale($sdate,$ponum,$sellId,$database)
 {
   $sql_query = "INSERT INTO SALE(purchase_order_num,date,user_id) VALUES (".$ponum.",\"".$sdate."\",".$sellId.")";
+  echo $sql_query;
   mysql_query($sql_query);
 }
 
@@ -40,7 +41,7 @@ executeSale($sdate,$ponum,$sellId,$database);
 
 foreach($items as $item)
 {
-  $sql_query = "UPDATE ITEM SET is_sold=".$sold." WHERE inventory_id=".$invId." AND serial_number=".$item;
+  //$sql_query = "UPDATE ITEM SET is_sold=".$sold." WHERE inventory_id=".$invId." AND serial_number=".$item;
   mysql_query($sql_query);
 }
 
