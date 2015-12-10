9801e5fc3623e420c6bb6cd4c1877eeb8c9f715c
bkolega <bkolega@Bills-MacBook-Pro.local>
2015-12-10 12:51:27 -0600

    fds:

--- js/update_acc.js
+++ js/update_acc.js
@@ -8,11 +8,11 @@ function updateAcc(uname, fn, ln, phoneNum, old, new1){
 		newpass1: new1
 		
 	},function(data, status){
-		if(data.length > 5){
-			$('#test').html(data);
+		if(data.length == 2){
+			alert("Update failed!");
 		}else{
 			if(data.length == 3){
-				window.location="my_account.html";
+				alert("Update successful!");
 				return true;
 			}else{
 				alert("Update failed!");
@@ -26,6 +26,20 @@ function updateAcc(uname, fn, ln, phoneNum, old, new1){
 $(document).ready(function(){
 	$('#submitUpdate').click(function(e){
 		var username = ($('#userID').val());
+		var oldpw = $('#opw').val();
+		var new1 = $('#npw1').val();
+		var new2 = $('#npw2').val();
+		var first = $('#fn').val();
+		var last = $('#ln').val();
+
+		if(new1 != new2)
+		{
+			e.preventDefault();
+			alert("New passwords do not match!");
+		} if(first == "" || last == "" || oldpw == "" || new1 == "" || new2 =="") {
+			e.preventDefault();
+			alert("Empty fields detected!");
+		}
 		/*if($('#addFname').val() == ""){
 			e.preventDefault();
 			alert("Missing Data: First Name. Please fill out.");
