d4de9610b7fbc4d795acc7239f1d3ffcf22a707c
bkolega <bkolega@Bills-MacBook-Pro.local>
2015-12-11 17:56:29 -0600

    update

--- js/my_reports.js
+++ js/my_reports.js
@@ -14,9 +14,35 @@ function createReport(us,p1, p2, p3, p4, p5, p6){
 	});
 }
 
+/*function getUserName(sess){
+  $.post("php/session.php",{
+    method: "retrieve",
+    session: sess,
+    user: null
+  })
+  .done(function(data){
+	  // On completeion, kicks off php to get quickstats and inventory overview of userID
+	  //loadReports(data);
+	var col1 = $('#c1').val();
+	var col2 = $('#c2').val();
+	var col3 = $('#c3').val();
+	var cond1 = $('#cond1').val();
+	var cond2 = $('#cond2').val();
+	var fname = $('#filename').val();
+	  createReport(user,col1,col2,col3,cond1,cond2,fname);
+	  return data;
+  });
+}*/
+
+
+
 $(document).ready(function(){
 	$('#submitReport').click(function(e){
-		var user = $('#userID').val()
+	        var cookie = document.cookie.split("=");
+	        var session = cookie[1].split(";");
+	        session = session[0];
+	        getUserName(session);
+		var user = $('#userID').val();
 		var col1 = $('#c1').val();
 		var col2 = $('#c2').val();
 		var col3 = $('#c3').val();
